a2ff1864e28f33dab24de874dd6f534a2f99015e
Will Metcalf <william.metcalf@gmail.com>
2017-10-06 20:11:57 -0500

    Use multiple processes for most expensive part of this tool. By default creates the same number of processes as detected CPU's

--- sellitbythengram.py
+++ sellitbythengram.py
@@ -11,6 +11,8 @@ import hashlib
 from optparse import OptionParser
 import string
 from collections import OrderedDict
+import multiprocessing
+
 try:
     import re2 as re
 except ImportError:
@@ -24,8 +26,13 @@ parser.add_option("-P", dest="i_re", type="string", help="Ngram must match this 
 parser.add_option("-v", dest="e_re", type="string", help="Ngram matching this regex will not be included in the output")
 parser.add_option("-g", dest="do_reass", help="Generate a regex string provide path to regex assemble perl script")
 parser.add_option("-r", dest="reduce", action='store_true', help="Reduce matches to globally unique")
+parser.add_option("--ssplit", dest="ssplit", action='store_true', help="space seperated ngrams instead of byte len. Useful for meaning for word groups")
+parser.add_option("--normwhite", dest="normwhite", action="store_true", help="normalize whitespace")
+parser.add_option("--lower", dest="lower", action="store_true", help="lowercase the input")
+parser.add_option("--nproc", dest="nproc", type=int, help="number of processes to use for (directory mode only) defaults to number of CPU's on system")
 (options, args) = parser.parse_args()
 
+PROCS = 1
 flist = []
 gcracker = OrderedDict() 
 nlist = set()
@@ -36,6 +43,7 @@ reass_in = set()
 reass = None
 replchars = re.compile('([^' + re.escape(string.letters + string.digits) +'])')
 replchars_nprint = re.compile('([^' + re.escape(string.printable) +'])')
+
 def cmd_wrapper(cmd):
     #print("running command and waiting for it to finish %s" % (cmd))
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
@@ -45,6 +53,45 @@ def cmd_wrapper(cmd):
 def replchars_to_hex(match):
     return r'\x{0:02x}'.format(ord(match.group()))
 
+if options.nproc and options.nproc > 0:
+    PROCS=options.nproc
+else:
+    PROCS=multiprocessing.cpu_count()
+
+def ngram_from_file(entry,nlist,key):
+    print("Generating n-grams for {0}".format(entry))
+    dapoop=open(entry).read()
+    if options.normwhite:
+        dapoop = re.sub(r'[\r\n\s\t]+',"\x20",dapoop)
+    if options.lower:
+        dapoop = dapoop.lower()
+    tmpd=OrderedDict()
+    for i in nlist:
+        tmpset=[]
+        if options.ssplit:
+            tgrams=ngrams(dapoop.split(" "), i)
+        else:
+            tgrams=ngrams(dapoop, i)        
+        for grams in tgrams:
+            if options.ssplit:
+                nstr=''' '''.join(grams)
+            else:
+                nstr=''''''.join(grams)
+            if nstr not in ngram_cnt:
+                ngram_cnt[nstr] = 1
+            else:
+                ngram_cnt[nstr] += 1
+            if nstr not in tmpset:
+                tmpset.append(nstr)
+            tmpd[i]=tmpset
+
+    tmpd2={}        
+    tmpd2["data"]=tmpd
+    tmpd2["key"]=key
+    return tmpd2
+
+def setg(tmpd2):
+    gcracker[tmpd2["key"]]=tmpd2["data"]
 
 if(options.dir):
     if os.path.isdir(options.dir):
@@ -58,6 +105,7 @@ if(options.nlist):
         blah=options.nlist.split(",")
         for entry in blah:
             nlist.add(int(entry))
+        nlist=list(nlist)
     except Exception as e:
         print("You got problems BRO %s" % (e))
         sys.exit(-1)    
@@ -66,7 +114,7 @@ else:
 
 if(options.i_re):
     try:
-        include_regex = re.compile(options.i_re)
+        include_regex = re.compile(options.i_re,re.S)
     except Exception as e:
         print("Error Compiling Include Regex" % (e))
         sys.exit(-1)
@@ -88,21 +136,12 @@ nlist.sort(reverse=True)
 
 if nlist and flist:
     if len(flist) > 1:
+        pool = multiprocessing.Pool(PROCS)
         for entry in flist:
-            dapoop=open(entry).read()
             key = os.path.basename(entry)
-            gcracker[key]=OrderedDict()
-            for i in nlist:
-                tmpset=set()
-                tgrams=ngrams(dapoop, i)
-                for grams in tgrams:
-                    nstr=''''''.join(grams)
-                    if nstr not in ngram_cnt:
-                        ngram_cnt[nstr] = 0
-                    else:
-                        ngram_cnt[nstr] += 1
-                    tmpset.add(''''''.join(grams))
-                gcracker[key][i]=tmpset
+            pool.apply_async(ngram_from_file,args=(entry,nlist,key),callback=setg)
+        pool.close()
+        pool.join()
 
 elif nlist and options.input_file and os.path.exists(options.input_file):
     lines=open(options.input_file).readlines()
@@ -115,22 +154,35 @@ elif nlist and options.input_file and os.path.exists(options.input_file):
     print("The longest line in the list is: %s." % (len(lines[-1])))
     key = 0
     for dapoop in lines:
+        if options.normwhite:
+            dapoop = re.sub(r'[\r\n\s\t]+',"\x20",dapoop)
+
         gcracker[key]=OrderedDict()
         for i in nlist:
             tmpset=set()
-            tgrams=ngrams(dapoop.strip(), i)
+            if options.ssplit:
+                tgrams=ngrams(dapoop.strip().split(), i)
+            else:
+                tgrams=ngrams(dapoop.strip(), i)
+
             for grams in tgrams:
-                tmpset.add(''''''.join(grams))
+                if options.ssplit:
+                    nstr=''' '''.join(grams)
+                else:
+                    nstr=''''''.join(grams)
             gcracker[key][i]=tmpset
         key = key + 1
 
-all_list = []
+all_list = set()
 if nlist and gcracker:    
     for i in nlist:
         setlst = []
+        lastset = []
         for key in gcracker:
             setlst.append(gcracker[key][i])        
-        s1=set.intersection(*setlst)
+            lastset = gcracker[key][i]
+        s1=set.intersection(*map(set,setlst))
+        s1=[t for t in setlst[0] if t in s1]
         cset=set()
         if options.reduce:
             tmp_set=set()
@@ -138,12 +190,11 @@ if nlist and gcracker:
                 found = False
                 for entry2 in all_list:
                     if entry in entry2:       
-    #                    print("skipping %s found in larger string %s" % (entry,entry2)) 
                         found = True
                         break
                 if not found:
                    tmp_set.add(entry)
-                   all_list.append(entry)
+                   all_list.add(entry)
             s1=tmp_set
         if s1:
             print("Ngrams of len:%s" % (i))
@@ -152,6 +203,7 @@ if nlist and gcracker:
                if exclude_regex and exclude_regex.search(entry) != None:
                    continue
                pprint(entry)
+               pprint(entry.encode('hex')) 
                print "content:\x22|"+" ".join(re.findall("(?P<foo>[a-f0-9]{2})",entry.encode("hex")))+"|\x22;"
                cset=cset.union(set(entry)) 
                if reass: 
@@ -159,6 +211,7 @@ if nlist and gcracker:
 
            elif not include_regex and exclude_regex and exclude_regex.search(entry) == None:
                pprint(entry)
+               pprint(entry.encode('hex'))
                print "content:\x22|"+" ".join(re.findall("(?P<foo>[a-f0-9]{2})",entry.encode("hex")))+"|\x22;"
                cset=cset.union(set(entry))
                if reass:     
@@ -166,6 +219,7 @@ if nlist and gcracker:
 
            elif not include_regex and not exclude_regex:
                pprint(entry)
+               pprint(entry.encode('hex'))
                print "content:\x22|"+" ".join(re.findall("(?P<foo>[a-f0-9]{2})",entry.encode("hex")))+"|\x22;"
                cset=cset.union(set(entry))
                if reass:
